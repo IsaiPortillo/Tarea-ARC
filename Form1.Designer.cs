4de5e17daa68594b0f49f90caabcbd68da6c8d47
Isai Portillo <PC18035@ues.edu.sv>
2023-08-31 12:02:50 -0600

    Agregando parte de conversiones
    Faltaría probar si da lo que hacemos con lo que nos dijo la Ing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataTypeComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.binaryOutputLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Elija lo que quiere realizar:";
+            // 
+            // dataTypeComboBox
+            // 
+            this.dataTypeComboBox.AllowDrop = true;
+            this.dataTypeComboBox.BackColor = System.Drawing.Color.White;
+            this.dataTypeComboBox.Location = new System.Drawing.Point(113, 43);
+            this.dataTypeComboBox.Name = "dataTypeComboBox";
+            this.dataTypeComboBox.Size = new System.Drawing.Size(126, 21);
+            this.dataTypeComboBox.TabIndex = 1;
+            this.dataTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.dataTypeComboBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese el Dato:";
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.valueTextBox.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.valueTextBox.Location = new System.Drawing.Point(113, 98);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(126, 20);
+            this.valueTextBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "La respuesta es:";
+            // 
+            // binaryOutputLabel
+            // 
+            this.binaryOutputLabel.Location = new System.Drawing.Point(12, 189);
+            this.binaryOutputLabel.Name = "binaryOutputLabel";
+            this.binaryOutputLabel.Size = new System.Drawing.Size(332, 85);
+            this.binaryOutputLabel.TabIndex = 5;
+            this.binaryOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(113, 138);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(126, 23);
+            this.convertButton.TabIndex = 6;
+            this.convertButton.Text = "Convertir";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(356, 283);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.binaryOutputLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.valueTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataTypeComboBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox dataTypeComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label binaryOutputLabel;
+        private System.Windows.Forms.Button convertButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,59 @@ namespace Tarea_ARC
         public Form1()
         {
             InitializeComponent();
+            InitializeComboBox();
+        }
+
+        private void dataTypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComboBox()
+        {
+            dataTypeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            dataTypeComboBox.Items.AddRange(new string[] { "Entero sin signo", "Entero con signo", "Flotante", "Carácter", "Cadena de caracteres" });
+            dataTypeComboBox.SelectedIndex = 0;
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = dataTypeComboBox.SelectedIndex;
+
+            switch (selectedIndex)
+            {
+                case 0: // Entero sin signo
+                    uint unsignedIntValue = uint.Parse(valueTextBox.Text);
+                    binaryOutputLabel.Text = Convert.ToString(unsignedIntValue, 2).PadLeft(16, '0');
+                    break;
+
+                case 1: // Entero con signo
+                    short signedIntValue = short.Parse(valueTextBox.Text);
+                    binaryOutputLabel.Text = Convert.ToString(signedIntValue, 2).PadLeft(16, signedIntValue < 0 ? '1' : '0');
+                    break;
+
+                case 2: // Flotante (simplificado)
+                    float floatValue = float.Parse(valueTextBox.Text);
+                    byte[] floatBytes = BitConverter.GetBytes(floatValue);
+                    string binaryRepresentation = string.Join("", floatBytes);
+                    binaryOutputLabel.Text = binaryRepresentation;
+                    break;
+
+                case 3: // Carácter
+                    char charValue = valueTextBox.Text[0];
+                    binaryOutputLabel.Text = Convert.ToString(charValue, 2).PadLeft(16, '0');
+                    break;
+
+                case 4: // Cadena de caracteres
+                    string stringValue = valueTextBox.Text;
+                    string binaryString = "";
+                    foreach (char c in stringValue)
+                    {
+                        binaryString += Convert.ToString(c, 2).PadLeft(16, '0');
+                    }
+                    binaryOutputLabel.Text = binaryString;
+                    break;
+            }
         }
     }
 }
